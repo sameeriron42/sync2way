bf45bd87fe9deb6e57b95e6f8b3323851666fe7e
sameeriron42 <sameermark42@gmail.com>
2023-04-14 16:08:38 +0530

    added stripe integration

--- app/queues/producer.py
+++ app/queues/producer.py
@@ -1,15 +1,18 @@
 import pika
-import json
 from routers import models
 
 def publish_to_queue(record: models.Customer):
-    print("at publish")
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='tst')
-    print("before dict")
     msg= record.json()
-    print(f"after dict {msg}")
-    channel.basic_publish(exchange='',routing_key='tst',body=msg)
-    print('msg sent')
+    channel.basic_publish(exchange='',
+                          routing_key='tst',
+                          body=msg,    
+                          properties=pika.BasicProperties(
+        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+    ))
+
+    print('msg sent to queue')
+    print(msg)
     connection.close()
--- app/queues/stripe_worker.py
+++ app/queues/stripe_worker.py
@@ -1,12 +1,29 @@
 import pika
+import stripe
+from dotenv import load_dotenv
+import os
+import json
 
+
+load_dotenv()
+stripe.api_key = os.getenv('STRIPE_RESTRICTED_KEY')
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare('tst')
 
-def handleMsg(ch,method,props,body):
-    print(body.decode())
+def handleMsg(ch,method,props,body:bytes):
+    body = json.loads(body.decode())
+    print(body)
+    try:
+        stripe.Customer.create(name=body["name"],email=body["email"],metadata={"id":body["id"]})
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_consume(queue='tst',on_message_callback= handleMsg,auto_ack=True)
+    except stripe.error.InvalidRequestError as e:
+        print(f"Invalid Request Parameter: {e.param}")
+    except Exception as e:
+        print(f"an error occured: {e}")
+
+
+channel.basic_consume(queue='tst',on_message_callback= handleMsg)
 print('starting to consume...')
 channel.start_consuming()
