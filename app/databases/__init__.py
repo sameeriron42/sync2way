bb83782c7d9fe5cbc4413ab60d317ebadc9614fa
sameeriron42 <sameermark42@gmail.com>
2023-04-25 22:12:18 +0530

    added methods for factory function

--- app/databases/__init__.py
+++ app/databases/__init__.py
@@ -1,18 +1,20 @@
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-load_dotenv()
 
-SQL_URL = os.getenv("SQL_ENGINE_URI")
-sql_engine =create_engine(SQL_URL)
+def create_db_session(sql_uri:str):
+    sql_engine = get_sql_engine(sql_uri)
+    
+    #instance of this class represents database session, 
+    # naming it so to distinguish from Session
+    SessionLocal = sessionmaker(bind=sql_engine,autoflush=False)
+    return SessionLocal
 
-#instance of this class represents database session, 
-# naming it so to distinguish from Session
-SessionLocal = sessionmaker(bind=sql_engine,autoflush=False)
+def get_sql_engine(SQL_URI:str):
+    sql_engine =create_engine(SQL_URI)
+    return sql_engine
 
 #Base class is inherited later to create each of the db models(tables)
 Base = declarative_base()
