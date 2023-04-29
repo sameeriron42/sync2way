e88a3a034de786539cf0d7fa0254e04038ac0751
sameeriron42 <sameermark42@gmail.com>
2023-04-29 14:24:08 +0530

    post method success and faulure test

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,10 +1,25 @@
 import json
+from fastapi.testclient import TestClient
 
-def test_create_user(client):
+def test_create_user_success(client:TestClient):
+    """
+    Create New user in the DB
+    """
     data = {"name":"testUser","email":"test@email.com"}
     response_data = {"id":1,"name":"testUser","email":"test@email.com"}
 
     response = client.post("/customers",content=json.dumps(data))
-
     assert response.status_code == 200
     assert response.json()== response_data
+
+def test_create_user_failed(client:TestClient):
+    """
+    Trying to create with existing email
+    """
+    data = {"name":"testUser","email":"test@email.com"}
+
+    client.post("/customers",content=json.dumps(data))
+    response = client.post("/customers",content=json.dumps(data))
+
+    assert response.status_code==404
+    assert response.json() == {"detail":"Email already registered"}
